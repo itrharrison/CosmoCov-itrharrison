c5e0b9de46c9a929aa6cffff3e1f414c68f1bc23
Ian Harrison <itrharrison@gmail.com>
2023-08-29 10:55:03 +0100

    added sub script

---
+++ covs/sub_files/cov_desy3_fulltomo_g.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=desy3_g_cov
+#SBATCH --p c_compute_physics1
+#SBATCH --A scw1361
+#SBATCH --nodes=1
+#SBATCH --ntasks=28
+#SBATCH --array=0-55
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1gb
+#SBATCH --time=72:00:00
+#SBATCH --output=%A.out
+#SBATCH --error=%A.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=harrisoni@cardiff.ac.uk
+
+cd $SLURM_SUBMIT_DIR
+module load anaconda/2023.03
+module load compiler/gnu/8/1.0
+module load cfitsio
+module load mpi/openmpi
+module load gsl
+module load fftw
+module load git
+N=1540
+for ((i = 0; i < SLURM_NTASKS; i++)); do
+	i_cov=$((SLURM_ARRAY_TASK_ID*SLURM_NTASKS+i+1));
+	if [[ ${i_cov} -gt N ]]; then break; fi;
+	srun --ntasks 1 --exclusive -c 1 ./cov ${i_cov} ini_files/cov_desy3_fulltomo_g.ini >&/home/c.c1025819/CosmoCov-itrharrison/covs/log_files/cov_desy3_fulltomo_g_${i_cov}.log
+done;
+
+# N / ntasks = array
